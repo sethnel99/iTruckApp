7256ac67cf8696316063b2b2b6e91f10f2e1283e
Ross Epstein <rossepstein2013@u.northwestern.edu>
2012-05-01 23:50:41 -0500

    adding new map page to check-in. breaks whole thing

---
+++ NomadTruck/MapViewController.h
@@ -0,0 +1,13 @@
+//
+//  MapViewController.h
+//  NomadTruck
+//
+//  Created by Sandia Ren on 5/1/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MapViewController : UIViewController
+
+@end
